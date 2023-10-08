1b9dcd95707e9ebe4b6f9a142c5274098ac1dc29
Jdemule <ouldrabia.jamal@gmail.com>
2023-10-08 16:19:57 +0200

    bbb

--- src/main/java/com/example/product_api/ProductApiApplication.java
+++ src/main/java/com/example/product_api/ProductApiApplication.java
@@ -1,8 +1,12 @@
 package com.example.product_api;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductApiApplication { // Cette classe ce lance au serveur
 
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApiApplication.class, args);
+	}
 }
